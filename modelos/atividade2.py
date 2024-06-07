e77b03cf30e290012c8efb37282039b71c9b20ed
Helolara <heloise.lara@escola.pr.gov.br>
2024-06-07 12:33:29 -0300

    att nova

--- modelos/atividade2.py
+++ modelos/atividade2.py
@@ -1,4 +1,4 @@
-# Classe Carro
+#questão 1
 class Carro:
     def __init__(self, modelo, cor, ano):
         self.modelo = modelo
@@ -8,33 +8,32 @@ class Carro:
     def __str__(self):
         return f"Carro: {self.modelo}, Cor: {self.cor}, Ano: {self.ano}"
 
-# Criando uma instância da classe Carro
-carro1 = Carro('Augi', 'Preto', 2023) 
-print(carro1)
+carro_1 = Carro('Augi', 'Preto', 2023)
+print(carro_1)
 
-# Classe Restaurante
+#questão 2
 class Restaurante:
     def __init__(self, nome, categoria):
         self.nome = nome
         self.categoria = categoria
         self.ativo = False
-        self.endereco = None
-        self.capacidade = 100 
+        self.menu = None
+        self.mesas = 0
 
+#questão 4
     def __str__(self):
-        return f"Restaurante: {self.nome}, Categoria: {self.categoria}, Ativo: {self.ativo}, Endereço: {self.endereco}, Capacidade: {self.capacidade}"
-
-# Criando uma instância da classe Restaurante e atribuindo valores aos seus atributos
+        return f"Restaurante: {self.nome}, Categoria: {self.categoria}, Ativo: {self.ativo}, Menu: {self.menu}, Mesas: {self.mesas}"
+    
+#questão 3
 restaurante1 = Restaurante('Praça', 'Gourmet')
-restaurante1.endereco = 'Rua das Codornas, 165'
+restaurante1.endereco = 'Rua das Codornas, 145'
 restaurante1.capacidade = 100
 print(restaurante1)
-
-# Criando uma instância da classe Restaurante utilizando o construtor
+#construtor
 restaurante2 = Restaurante('La Rocca', 'Italiana')
 print(restaurante2)
 
-# Classe Cliente
+#questão 5
 class Cliente:
     def __init__(self, nome, email, telefone, endereco):
         self.nome = nome
@@ -45,11 +44,10 @@ class Cliente:
     def __str__(self):
         return f"Cliente: {self.nome}, Email: {self.email}, Telefone: {self.telefone}, Endereço: {self.endereco}"
 
-# Instanciando 3 objetos da classe Cliente e atribuindo valores aos seus atributos
-cliente1 = Cliente('João Lara', 'joaolara@gmail.com', '9958-1254', 'Rua Bom Jesus, 789')
-cliente2 = Cliente('Rosane Maria', 'rosanemaria@gmail.com', '8956-521', 'Rua Beija-flor, 125')
-cliente3 = Cliente('Gabriel Castro', 'gabrielcastro@gmail.com', '9887-4562', 'Rua Ouro Verde, 236')
+cliente1 = Cliente('Pedro Brecher', 'pedro.brecher@escola.pr.gov.br', '9258-7852', 'Rua Bom Jesus, 223')
+cliente2 = Cliente('Heloise de Castro', 'heloise.castro@escola.pr.gov.br', '9568-4562', 'Rua Beija Flor, 78')
+cliente3 = Cliente('Leticia Rompava', 'leticia.rompava@escola.pr.gov.br', '9984-2598', 'Rua Ouro Verde, 45')
 
 print(cliente1)
 print(cliente2)
-print(cliente3) 
+print(cliente3)
